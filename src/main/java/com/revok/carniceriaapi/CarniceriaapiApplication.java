0ba44707d52f3133779812ac3a6a6075471062a0
Arellano136 <20223tn095@utez.edu.mx>
2024-08-14 18:56:48 -0600

    Commit inicial

---
+++ src/main/java/com/revok/carniceriaapi/CarniceriaapiApplication.java
@@ -0,0 +1,13 @@
+package com.revok.carniceriaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarniceriaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarniceriaapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revok/carniceriaapi/CarniceriaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revok.carniceriaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarniceriaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
